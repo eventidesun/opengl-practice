3ee78b469ebc80585cd0b694b6cdc95fc91bd547
Vaishnavi Gudimella <118631877+eventidesun@users.noreply.github.com>
2025-04-22 13:29:22 -0400

    Update main.cpp

--- first-project/main.cpp
+++ first-project/main.cpp
@@ -1,1 +1,39 @@
+#include <glad/glad.h> // this has to be first
+#include <GLFW/glfw3.h> // this detects opengl/gl.h on macos automatically so the glad stuff has to be first
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World!", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
